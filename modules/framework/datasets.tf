9e5f3f2bafb7d3c0cfeaadee3f41c822e6ce7fb1
ivanvalbuena-dvt <ivan.valbuena@devoteam.com>
2024-12-23 16:06:01 +0100

    test

--- modules/framework/datasets.tf
+++ modules/framework/datasets.tf
@@ -1,10 +1,7 @@
 
 # DATASETS
-/*resource "google_bigquery_dataset" "common_datasets" {
-  for_each      = local.common_datasets
-  location      = local.location
-  dataset_id    = "d_${local.project_name}_${each.key}_${lower(local.location)}_${lower(local.environment)}"
-  friendly_name = "d_${local.project_name}_${each.key}_${lower(local.location)}_${lower(local.environment)}"
-  description   = each.value
+resource "google_bigquery_dataset" "common_datasets" {
+  location      = "europe-west1"
+  dataset_id    = "d_cicd_setup"
+  friendly_name = "cicd_setup"
 }
-*/
